3e792c3d4b82a67f1e1eb7f95ee55e65fd0c582b
Outdam Meas <mstdam@gmail.com>
2022-02-12 15:34:02 +0800

    create ssl cert via terraform

--- main.tf
+++ main.tf
@@ -1,21 +1,33 @@
 provider "aws" {
-  // for acm_certificate_domain only
+  # cloudfront requires ssl cert to be in us-east-1
   region = "us-east-1"
 }
 
 locals {
-  lambda_function_association = {
-    event_type : "origin-request"
-    include_body : false
-    lambda_arn : module.index_routing_handler_at_edge.arn
-    #    lambda_arn : "arn:aws:lambda:us-east-1:883979811993:function:cloudfront-index-handler:1"
-  }
+  domain = "oudam.es"
 }
 
 module "cloudfront_s3_website_oudam_es" {
   source                      = "./terraform-aws-cloudfront-s3-website"
-  hosted_zone                 = "oudam.es"
-  domain_name                 = "oudam.es"
-  acm_certificate_domain      = "oudam.es"
-  lambda_function_association = local.lambda_function_association
+  hosted_zone                 = local.domain
+  domain_name                 = local.domain
+  acm_certificate_domain      = local.domain #aws_acm_certificate.oudam_es_ssl_cert.domain_name
+  lambda_function_association = {
+    event_type : "origin-request"
+    include_body : false
+    lambda_arn : module.index_routing_handler_at_edge.arn
+  }
+}
+
+resource "aws_acm_certificate" "oudam_es_ssl_cert" {
+  domain_name       = local.domain
+  validation_method = "DNS"
+
+  tags = {
+    flows = "cloudfront:s3"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
